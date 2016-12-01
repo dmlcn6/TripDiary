eb2aba34068e3d7ea61ef360c6a3bf15bd2a5155
Darryl Lopez <Lopez@Darryls-MacBook-Pro.local>
2016-11-30 23:01:33 -0600

    stuffed the entire photo picker controller inside AddTripViewController

--- TripEx/AddTripViewController.swift
+++ TripEx/AddTripViewController.swift
@@ -9,53 +9,103 @@
 import UIKit
 import CoreData
 
-class AddTripViewController: UIViewController {
-
+class AddTripViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var currUser:User?
     var trip:Trip?
     
     @IBOutlet weak var titleText: UITextField!
     @IBOutlet weak var locationText: UITextField!
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+    var userPickedImage: UIImage?
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
         
-        
-        //Add Save button
+        //Add NEXT button that saves a Trip
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(AddTripViewController.saveTrip))
-
-        //navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(AddTripViewController.saveTrip))
+        
+    }
+    
+    //presents the photoLibrary for selecting without editing
+    func openPhotoLibrary() {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
     }
 
+    //BUTTON CLICKED TO PRESENT PHOTOLIBRARY
+    @IBAction func presentPhotoLibrary(_ sender: Any) {
+         openPhotoLibrary()
+    }
+    
+    //IF THE USER PICKS AN IMAGE
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        //grab the users selected image from the PhotoLibrary
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            userPickedImage = pickedImage
+            coverImageView.contentMode = .scaleAspectFit
+            coverImageView.image = userPickedImage
+            
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //User cancels the PhotoLibrary.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        //after cancelling a picking, pop
+        // to AddTripViewController
+        /*
+         if let navigationController = navigationController{
+            let viewControllers: [UIViewController] = navigationController.viewControllers
+            for aViewController in viewControllers {
+                if(aViewController is AddTripViewController){
+                    navigationController.popToViewController(aViewController, animated: true);
+                }
+            }
+         }
+         */
     }
     
+    //If the User hits the NEXT button in Navigation
     func saveTrip(){
         let context = DatabaseController.getContext()
+        
         
         //new trip
         if trip == nil{
             trip = Trip(context: context)
         }
         
-        //old task
+        //old trip
         if let trip = trip{
             trip.tripTitle = titleText.text!
             trip.tripLocation = locationText.text!
             trip.tripLatitude = 0
             trip.tripLongitude = 0
-            
             //trip.tripMemories
+            
+            //fetch trip cover photo from bundle
             
             
             do{
                 try context.save()
                 
-                performSegue(withIdentifier: "add", sender: self )
+                performSegue(withIdentifier: "add", sender: self)
             }catch let error as NSError {
                 print("Error Saving \(error.userInfo)")
             }
@@ -71,7 +121,6 @@ class AddTripViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         
-        
         if segue.identifier == "add" {
             //saveTrip()
             print("it saved")
@@ -79,14 +128,6 @@ class AddTripViewController: UIViewController {
             if let dest = segue.destination as? AddMemoryViewController{
                 dest.parentTrip = trip
             }
-        }
-        else if segue.identifier == "show" {
-            print("it showed")
-            
-            if let dest = segue.destination as? TripViewController{
-                dest.title = "Trip Cover Photo"
-            }
-
         }
     
     }
