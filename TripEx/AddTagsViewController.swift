1a1e105bd7ad6edbc479961016eec79509a0f740
Darryl Lopez <dmlcn6@mail.missouri.edu>
2016-12-09 17:01:51 -0600

    added AddTags ViewCont

---
+++ TripEx/AddTagsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddTagsViewController.swift
+//  TripEx
+//
+//  Created by Darryl Lopez on 12/9/16.
+//  Copyright © 2016 Darryl Lopez. All rights reserved.
+//
+
+import UIKit
+
+class AddTagsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
