40cc947b615a23ea0beb2cf44fd0bd77ff047e9e
Jeffrey Freet <jeffreyfreet@yahoo.com>
2016-12-11 17:30:16 -0600

    Pin functionality added to Map

--- TripEx/ProfileViewController.swift
+++ TripEx/ProfileViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class ProfileViewController: UIViewController {
     
@@ -17,12 +18,15 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     var currUser: User?
+    var trips: [Trip]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //changes color of the nav bar at the top
         self.navigationController?.navigationBar.barTintColor = UIColor(red:0.35, green:0.78, blue:0.98, alpha:1.0)
+        
+        addTripToMap()
     }
     
     
@@ -30,6 +34,7 @@ class ProfileViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         if let currUser = currUser, currUser.isLoggedIn == true{
             print("\n\nPROFILE \(currUser.userName) logged in \(currUser)\n\n")
+            fetchData()
         }else {
             print("\n\nProfile Not logged or nil\n\n")
         }
@@ -50,21 +55,58 @@ class ProfileViewController: UIViewController {
         
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func addTripToMap() {
+        if let trips = trips{
+            for trip in trips {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = CLLocationCoordinate2DMake(trip.tripLatitude, trip.tripLongitude)
+                annotation.title = trip.tripTitle
+                //annotation.subtitle = "815 Olive Street"
+                mapView.addAnnotation(annotation)
+                
+            }
+        }
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2DMake(38, -92)
+        //annotation.title = trip.tripTitle
+        //annotation.subtitle = "815 Olive Street"
+        mapView.addAnnotation(annotation)
     }
-    */
-
+    
+    func fetchData() {
+        let fetch = NSFetchRequest<Trip>(entityName: "Trip")
+        
+        //let predicate = NSPredicate(format: "tripTitle == %@", "*")
+        
+        //fetch.predicate = predicate
+        //fetch.fetchLimit = 2
+        
+        do{
+            trips = try DatabaseController.getContext().fetch(fetch)
+        }catch let error as NSError {
+            print(" hello error \(error.userInfo)")
+        }
+        
+        //collectionView.reloadData()
+        
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
